a0f50e7a9f87f5d4674fad918562fcc5c5bcb489
jakedrubin <jakedrubin@gmail.com>
2025-04-16 15:40:16 -0400

    read the json header to show

--- backend/public/game_mode/show.php
+++ backend/public/game_mode/show.php
@@ -3,7 +3,9 @@
 <?php
 // backend/public/gamemode/show.php?id=1
 // GET one game mode by ID
+header('Content-Type: application/json');
 require_once('../../private/initialize.php');
+
 
 if(is_get_request() && isset($_GET['id'])) {
   $mode = find_game_mode_by_id($_GET['id']);
