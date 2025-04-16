b208339928ba14e78ed7cd9369800fd47d13e847
mxhng <67926586+mxhng@users.noreply.github.com>
2025-04-16 13:16:02 -0400

    Update db_credentials.php with max's credentials

--- backend/private/db_credentials.php
+++ backend/private/db_credentials.php
@@ -1,8 +1,14 @@
 <?php
 
+// define("DB_SERVER", "mysql.cise.ufl.edu");
+// define("DB_USER", "jakerubin");
+// define("DB_PASS", "newDBpw42_");
+// define("DB_NAME", "chainReaction");
+
+// Max's credentials
 define("DB_SERVER", "mysql.cise.ufl.edu");
-define("DB_USER", "jakerubin");
-define("DB_PASS", "newDBpw42_");
-define("DB_NAME", "chainReaction");
+define("DB_USER", "menghuahuang");
+define("DB_PASS", "maxmax123");
+define("DB_NAME", "internet_programming");
 
 ?>
