25781b2451fa93cfa0d5f9f068666b5f8356c71c
jakedrubin <jakedrubin@gmail.com>
2025-04-16 16:52:53 -0400

    revert some chagnes to confirm funcitonality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
-import { Game, GameMode } from '../types'; 
+import { GameMode } from '../types'; 
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
@@ -27,13 +27,14 @@ const LoginPage = () => {
     };
 
     const GameMode = () => {
-        const [gm, setGm] = useState<Game | null>(null);
+        const [gm, setGm] = useState<GameMode | null>(null);
       
         useEffect(() => {
           const fetchGm = async () => {
             try {
-              const response = await fetch(`./backend/public/game/show.php?id=1`);
+              const response = await fetch(`./backend/public/game_mode/show.php?id=1`);
               const data = await response.json();
+              console.log(data)
               if (!response.ok) throw new Error(data.error || 'Failed to fetch game mode');
               setGm(data);
             } catch (error) {
@@ -44,7 +45,7 @@ const LoginPage = () => {
           fetchGm();
         }, []);
       
-        return <h2>{gm ? gm.score : 'Loading...'}</h2>;
+        return <h2>{gm ? gm.mode_name : 'Loading...'}</h2>;
       };
 
     return (
