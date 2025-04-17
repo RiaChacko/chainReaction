6218d92b0e1354b9b62cc8a1fcf15b06555594f8
Ria Chacko <136252336+RiaChacko@users.noreply.github.com>
2025-04-16 20:24:18 -0400

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
 import { Game } from '../types'; 
-import { resolve } from 'path';
 
 const LoginPage = () => {
     const [username, setUsername] = useState('');
