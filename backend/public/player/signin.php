f677fba9c0d71686976e187c3223144f5c77ed02
jakedrubin <jakedrubin@gmail.com>
2025-04-17 00:21:37 -0400

    signin

--- backend/public/player/signin.php
+++ backend/public/player/signin.php
@@ -6,9 +6,7 @@ ini_set('display_errors', 1);
 header('Content-Type: application/json');
 require_once('../../private/initialize.php');
 
-$input = file_get_contents('php://input');
-$data = json_decode($input, true);
-
+$data = json_decode(file_get_contents("php://input"), true);
 $email = $data['email'] ?? null;
 $username = $data['username'] ?? null;
 $password = $data['password'] ?? null;
@@ -17,10 +15,10 @@ if ($email && $username && $password) {
     // Example: create player or handle logic
     // $result = create_player($email, $username, $password);
     $player = player_signin($email, $username, $password);
-    if (!$player) {
-        echo json_encode(['error' => 'Invalid email, username, or password']);
-        exit;
-    }
+    // if (!$player) {
+    //     echo json_encode(['error' => 'Invalid email, username, or password']);
+    //     exit;
+    // }
     $id = $player['player_id'] ?? null;
     echo json_encode([
         'id' => $id
