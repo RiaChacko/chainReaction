3e308a561637426dd56b6a5660c7e1589646af3b
jakedrubin <jakedrubin@gmail.com>
2025-04-17 04:15:56 -0400

    cosmetics and testing

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -22,6 +22,31 @@ const HomePage = () => {
             <h2 className="subtitle">Welcome, {username}</h2>
 
             <button onClick={() => handleScoreboardSelect()}>Scoreboard</button>
+
+            <button onClick={async () => {
+                try{
+                    const response = await fetch(`./backend/public/game/new.php`,{
+                        method:'POST',
+                        headers:{
+                            'Content-Type':'application/json',
+                        },
+                        body: JSON.stringify({
+                            game_mode_id: 1,                  // existing game_mode_id
+                            player_id: 1,                     // existing player_id
+                            start_time: "2025-04-17 10:00:00", // must be valid DATETIME
+                            end_time: "2025-04-17 10:10:00",   // must be valid DATETIME
+                            date: "2025-04-17",               // must be valid DATE
+                            score: 1000
+                          })                          
+                    });
+                    const data = await response.json();
+                    console.log(data);                
+        
+                } catch(error){
+                    
+                    console.log(error);
+                }
+            }}>Scoreboard</button>
             
             <h2 className="subtitle">Choose Mode</h2>
             <div className="mode-buttons">
