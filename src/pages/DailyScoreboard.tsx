ffa3ffb40b5392c7004827a94030ecf0f1ccfe7f
Dayanita <dayanitakumar@ufl.edu>
2025-04-16 22:45:35 -0400

    Create scoreboard page

---
+++ src/pages/DailyScoreboard.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import './DailyScoreboard.css';
+import { useNavigate } from 'react-router-dom';
+
+
+
+const DailyScoreboard = () => {
+    const navigate = useNavigate();
+    //const entries = loadScoreboard();
+    const [entries, setEntries] = useState([{date:"",score:0}]);
+    const loadScoreboard = async () => {
+        try{
+        const response = await fetch("./backend/public/daily_scoreboard/index.php");
+        console.log(response.text());
+        const data = await response.json();}
+        
+        //setEntries(data);}
+                  catch(error){console.error("Error getting scoreboard", error)}
+        
+        //setEntries(entries.filter((entry)=>(entry.date==("04-16-2025"))));
+        //setEntries(entries.sort((a,b)=>(a.score-b.score)));
+                  
+    }
+    
+    useEffect(()=>{
+        loadScoreboard();
+        
+    //setEntries(todayEntries);
+    },[]);
+    const handleBack = () => {
+        navigate('/home');
+    };
+    
+        
+
+    return (
+        <div className="letter-container">
+            <button onClick={handleBack} className="back-btn">← Back</button>
+            <h1 className="title">Daily Scoreboard</h1>
+            <ol>
+            {entries&&entries.map((entry)=>(
+                <li key={entry.score}>{entry.date} {entry.score}</li>
+            ))}
+            </ol>
+            
+
+        </div>
+    );
+};
+
+export default DailyScoreboard;
