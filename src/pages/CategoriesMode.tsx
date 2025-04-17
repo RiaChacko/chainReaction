6413719e5a370edcac3f24cb9865ec0ace30c0b6
mxhng <menghuamh@gmail.com>
2025-04-17 12:36:25 -0400

    fix unused useRef - eslint error

--- src/pages/CategoriesMode.tsx
+++ src/pages/CategoriesMode.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './CategoriesMode.css';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
